2e9af4a4e0569d1e10ad8f8a9707a001215032c5
Devin Garcia <devinmgarcia@gmail.com>
2021-08-27 15:17:59 -0500

    stop please

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,66 @@
 // initialize the tool-tip plugin for Bootstrap4
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-
+	$('[data-toggle="tooltip"]').tooltip();
+});
 
 $.ajax({
-  url: "data/cohort.json"
-}).done(cohortMembers)
-  .fail(function (error) {
-    console.log("error", error);
-  });
+	url: "data/cohort.json",
+})
+	.done(cohortMembers)
+	.fail(function (error) {
+		console.log("error", error);
+	});
 
 function cohortMembers(list) {
-  let data = list.cohort;
-  data.forEach(function (item) {
-    let studentContact = `<div class="studentContact">`
-    //if student doesn't have a portfolio site then don't display the icon
-    if (item.portfolio != null) {
-
-      studentContact += `<a href=${item.portfolio} target="_blank">
+	let data = list.cohort;
+	data.forEach(function (item) {
+		let studentContact = `<div class="studentContact">`;
+		//if student doesn't have a portfolio site then don't display the icon
+		if (item.portfolio != null) {
+			studentContact += `<a href=${item.portfolio} target="_blank">
       <i class="fas fa-globe fa-2x contactIcons"></i>
-      </a>`
-    }
-    //if student doesn't have a github site then don't display the icon
-    if (item.github != null) {
-
-      studentContact += `<a href=${item.github} target="_blank">
+      </a>`;
+		}
+		//if student doesn't have a github site then don't display the icon
+		if (item.github != null) {
+			studentContact += `<a href=${item.github} target="_blank">
       <i class="fab fa-github fa-2x contactIcons"></i>
-      </a>`
-    }
-    //if student doesn't have a linkedin site then don't display the icon
-    if (item.linkedIn != null) {
-
-      studentContact += `<a href=${item.linkedIn} target="_blank">
+      </a>`;
+		}
+		//if student doesn't have a linkedin site then don't display the icon
+		if (item.linkedIn != null) {
+			studentContact += `<a href=${item.linkedIn} target="_blank">
       <i class="fab fa-linkedin fa-2x contactIcons"></i>
-      </a>`
-    }
-    //if student doesn't have an email then don't display the icon
-    if (item.email != null) {
-
-      studentContact += `<a href=mailto:${item.email}>
+      </a>`;
+		}
+		//if student doesn't have an email then don't display the icon
+		if (item.email != null) {
+			studentContact += `<a href=mailto:${item.email}>
               <i class="fas fa-envelope fa-2x contactIcons"></i>
-            </a>`
-    }
-    studentContact += `</div>`
+            </a>`;
+		}
+		studentContact += `</div>`;
 
-    let studentInfo = `<div class="col-md-3 cohortMems">
+		let studentInfo = `<div class="col-md-3 cohortMems">
           <img class="card-img-top" src="images/classmates/${item.proImg}" alt="${item.firstName} ${item.lastName}" data-toggle="modal" data-target="#cohortMember${item.id}" style="cursor:pointer;">
           <div class="card-body">
-            <h4 class="card-title title-font">${item.firstName} ${item.lastName}</h4>`
-    //if student didn't provide a reelthemin quote then nothing is displayed
-    if (item.reelThemIn != null) {
-      studentInfo += `<p class="card-text">${item.reelThemIn}</p>`
-    }
-    studentInfo += studentContact
+            <h4 class="card-title title-font">${item.firstName} ${item.lastName}</h4>`;
+		//if student didn't provide a reelthemin quote then nothing is displayed
+		if (item.reelThemIn != null) {
+			studentInfo += `<p class="card-text">${item.reelThemIn}</p>`;
+		}
+		studentInfo += studentContact;
 
-    //if a student doesn't have a bio, then the learn more button doesn't appear and a modal isn't created
-    if(item.bio != null){
-
-    studentInfo += `
+		//if a student doesn't have a bio, then the learn more button doesn't appear and a modal isn't created
+		if (item.bio != null) {
+			studentInfo += `
             <center><button type="button" class="btn btn-outline-primary title-font bottom" data-toggle="modal" data-target="#cohortMember${item.id}">
            Learn More!
           </button></center>
           </div>
-        </div>`
-    //modal info
-    studentInfo +=`
+        </div>`;
+			//modal info
+			studentInfo += `
         <div class="modal fade" id="cohortMember${item.id}" tabindex="-1" role="dialog" aria-labelledby="cohortMember${item.id}Label" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -78,12 +73,11 @@ function cohortMembers(list) {
             <div class="modal-body">
             <center><img class="funImage" src="images/classmates/${item.funImg}" alt="${item.firstName} ${item.lastName} fun"/></center><br>
 
-            `
+            `;
 
-    studentInfo += studentContact
+			studentInfo += studentContact;
 
-
-    studentInfo += `
+			studentInfo += `
       
     ${item.bio}
     </div>
@@ -94,32 +88,32 @@ function cohortMembers(list) {
           </div >
         </div >
       </div > `;
-    } else {
-      studentInfo += `
+		} else {
+			studentInfo += `
       </div>
         </div>
-        `
-    }
-    document.getElementById("cohort").innerHTML += studentInfo;
-
-  });
-};
-
+        `;
+		}
+		document.getElementById("cohort").innerHTML += studentInfo;
+	});
+}
 
 $.ajax({
-  url: "data/techs.json"
-}).done(techs)
-  .fail(function (error) {
-    console.log("error", error);
-  });
+	url: "data/techs.json",
+})
+	.done(techs)
+	.fail(function (error) {
+		console.log("error", error);
+	});
 
 function techs(list) {
-  let data = list.techs;
-  data.forEach(function (item) {
-    document.getElementById("techs").innerHTML +=
-      `<div class="col-sm-2 technologies">
+	let data = list.techs;
+	data.forEach(function (item) {
+		document.getElementById(
+			"techs"
+		).innerHTML += `<div class="col-sm-2 technologies">
          <center><a href="${item.link}" target="_blank"><img class="techs" src="images/techs/${item.image}" alt="${item.name}" data-toggle="tooltip" data-placement="top" title="${item.name}"></a><br>
          </center>
       </div>`;
-  });
-};
+	});
+}
